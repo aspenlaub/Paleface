b02bb9c94a4274111799fa23aee8fc5bcaf44cff
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-09-28 16:14:14 +0200

    Versioning not working

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,22 +1,25 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-// [assembly: AssemblyTitle("Aspenlaub.Net.GitHub.CSharp.Paleface")]
-// [assembly: AssemblyDescription("")]
-// [assembly: AssemblyConfiguration("")]
-// [assembly: AssemblyCompany("")]
-// [assembly: AssemblyProduct("Aspenlaub.Net.GitHub.CSharp.Paleface")]
-// [assembly: AssemblyCopyright("Copyright ©  2019")]
-// [assembly: AssemblyTrademark("")]
-// [assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Aspenlaub.Net.GitHub.CSharp.Paleface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aspenlaub.Net.GitHub.CSharp.Paleface")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-// [assembly: ComVisible(false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-// [assembly: Guid("56bac911-db57-49bd-9c1a-0b2b9ff27230")]
+[assembly: Guid("56bac911-db57-49bd-9c1a-0b2b9ff27230")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,5 +28,6 @@
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
