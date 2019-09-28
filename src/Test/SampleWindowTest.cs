0a32c1b062557531c1c526c1948c6db0fa8f657d
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-09-28 16:09:24 +0200

    Test case failed in Release

--- src/Test/SampleWindowTest.cs
+++ src/Test/SampleWindowTest.cs
@@ -6,7 +6,10 @@ namespace Aspenlaub.Net.GitHub.CSharp.Paleface.Test {
     public class SampleWindowTest {
         protected static readonly string SampleWindowExecutable = typeof(SampleWindowTest).Assembly.Location
             .Replace(@"\Test\", @"\SampleWindow\")
-            .Replace("Aspenlaub.Net.GitHub.CSharp.Paleface.Test.dll", @"netcoreapp3.0\Aspenlaub.Net.GitHub.CSharp.Paleface.SampleWindow.exe");
+            .Replace("Aspenlaub.Net.GitHub.CSharp.Paleface.Test.dll",
+                typeof(SampleWindowTest).Assembly.Location.Contains("Debug")
+                    ? @"netcoreapp3.0\Aspenlaub.Net.GitHub.CSharp.Paleface.SampleWindow.exe"
+                    : @"Aspenlaub.Net.GitHub.CSharp.Paleface.SampleWindow.exe");
         protected static readonly string SampleWindowTitle = SampleWindowResources.WindowTitle;
 
         [TestMethod]
