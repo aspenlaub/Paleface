01ab53df12fddb3db35c2a499c5bbb56e037dd2e
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-08 20:45:02 +0200

    Make Window a standard ui class name

--- src/UiClassNames.cs
+++ src/UiClassNames.cs
@@ -2,12 +2,13 @@
 namespace Aspenlaub.Net.GitHub.CSharp.Paleface {
     public static class UiClassNames {
         public const string
+            ApplicationFrameWindow = "ApplicationFrameWindow",
             Button = "Button",
             ComboBox = "ComboBox",
             Edit = "Edit",
             Group = "Group",
             TextBox = "TextBox",
             NamedContainerAutomationPeer = "NamedContainerAutomationPeer",
-            ApplicationFrameWindow = "ApplicationFrameWindow";
+            Window = "Window";
     }
 }
